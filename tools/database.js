ce7b7362af27a2084cf4764a19a8128d71b583a4
joshNull <josh@joshuas-MacBook-Pro.local>
2020-03-19 19:31:06 +0800

    Established database connection and config

---
+++ tools/database.js
@@ -0,0 +1,22 @@
+const database = require("mysql2")
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const connection = database.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USERNAME,
+    database: process.env.DATABASE_NAME,
+    password: process.env.DATABASE_PASSWORD
+});
+
+const query = (query, parameters) => {
+    return new Promise((res, rej) => {
+        connection.query(query, parameters, function (error, results, fields) {
+            if (error) rej(error)
+            res(results)
+        });
+    })
+}
+
+module.exports = query
