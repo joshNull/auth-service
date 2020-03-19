82683209a3bbcd85ceff3cf4f8aaf7154c916ea1
joshNull <josh@joshuas-MacBook-Pro.local>
2020-03-19 21:36:35 +0800

    Code Consistency

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express")
 const app = express()
-
 const routes = require('./routes')
 
+
+app.use(express.json())
+
 app.use('/api/user', routes)
+
 
 module.exports = app
--- tools/database.js
+++ tools/database.js
@@ -1,4 +1,4 @@
-const database = require("mysql2")
+const database = require('mysql2')
 const dotenv = require('dotenv')
 
 dotenv.config()
