eea5c7c8d6ce95a043c16fd14d252e663df62637
joshNull <josh@joshuas-MacBook-Pro.local>
2020-03-20 16:20:23 +0800

    Added new model to get a user

--- model/userModel.js
+++ model/userModel.js
@@ -1,5 +1,14 @@
 const { database } = require("../tools")
 
+const getUser = async (data) => {
+    try {
+        let result = await database.query("SELECT * FROM user where ?", data)
+        return result
+    } catch (error) {
+        throw error
+    }
+}
+
 const createUser = async (data) => {
     try {
         let result = await database.query("INSERT INTO user SET ?", data)
@@ -10,5 +19,6 @@ const createUser = async (data) => {
 }
 
 module.exports = {
+    getUser,
     createUser
 }
