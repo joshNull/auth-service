1dcd44c7bb2a3d57372c6df408b99b3345255721
joshNull <josh@joshuas-MacBook-Pro.local>
2020-03-22 09:28:46 +0800

    Created Controller

---
+++ controller/userController.js
@@ -0,0 +1,68 @@
+const bcrypt = require("bcryptjs")
+const jwt = require('jsonwebtoken')
+const dotenv = require('dotenv')
+
+const { userModel } = require('.././model')
+const { userSchema } = require('.././validation')
+
+dotenv.config()
+
+const register = async (req, res) => {
+    try {
+        // Validate request
+        let { error } = userSchema.registerValidation(req.body)
+        if (error) throw { "error_message": error.details[0].message }
+
+        //Check if username exist
+        let user = await userModel.getUser({ "username": req.body.username })
+        if (user.length > 0) throw { "error_message": "Username already exist" }
+
+        //Hash password
+        const salt = await bcrypt.genSalt(10)
+        const password = await bcrypt.hash(req.body.password, salt)
+
+        //Create user
+        let result = await userModel.createUser({
+            username: req.body.username,
+            password: password
+        })
+
+        res.json(result)
+
+    } catch (error) {
+        console.log("ERROR : ", error)
+        res.status(400).json(error)
+    }
+
+}
+
+const login = async (req, res) => {
+    try {
+        // Validate request
+        let { error } = userSchema.registerValidation(req.body)
+        if (error) throw { "error_message": error.details[0].message }
+
+        let user = await userModel.getUser({ "username": req.body.username })
+
+        //Check if username don't exist
+        if (user.length <= 0) throw { "error_message": "Email don't exist" }
+
+        //Validate login
+        const validatePassword = await bcrypt.compare(req.body.password, user[0].password)
+        if (!validatePassword) throw { "error_message": "Password incorrect" }
+
+        //Create token
+        const token = jwt.sign({ id: user[0].id }, process.env.TOKEN_SECRET)
+
+        res.header('Authorization', token).json(user)
+
+    } catch (error) {
+        console.log("ERROR : ", error)
+        res.status(400).json(error)
+    }
+}
+
+module.exports = {
+    register,
+    login
+}
