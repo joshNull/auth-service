e3bf772628cdfff63eb58384daee553f45bb645d
joshNull <josh@josh.local>
2020-03-25 16:26:17 +0800

    Applied cookie parser as middleware

--- api/app.js
+++ api/app.js
@@ -1,11 +1,12 @@
 const express = require("express")
+const cookieParser = require('cookie-parser')
 const app = express()
 const routes = require('./routes')
 
 const { swaggerUIServe, swaggerUISetup } = require('./docs/swagger')
 
-
 app.use(express.json())
+app.use(cookieParser())
 
 app.use('/api/user', routes.user)
 
