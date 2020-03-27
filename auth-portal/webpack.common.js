d9f2e4d099393ea113e07804c4428b394ff63c76
joshNull <josh@josh.local>
2020-03-27 22:05:05 +0800

    Created public directory

--- auth-portal/webpack.common.js
+++ auth-portal/webpack.common.js
@@ -27,7 +27,7 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             filename: 'index.html',
-            template: "./src/index.html"
+            template: "./public/index.html"
         })
     ]
 }
