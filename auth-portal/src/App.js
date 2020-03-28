28b7803a5e9c0bf2ebda498be901d35df0e289e1
joshNull <josh@josh.local>
2020-03-28 13:31:08 +0800

    Fixed routes and added User, Role and Permission pages

--- auth-portal/src/App.js
+++ auth-portal/src/App.js
@@ -9,13 +9,6 @@ export default function App() {
     return (
         <ThemeProvider theme={theme}>
             <Router>
-                {/* <div>
-                    <ul>
-                        <li><Link to="/">PAGE</Link></li>
-                        <li><Link to="/page-one">PAGE 1</Link></li>
-                        <li><Link to="/page-two">PAGE 2</Link></li>
-                    </ul>
-                </div> */}
                 <Routes />
             </Router>
         </ThemeProvider>
--- auth-portal/src/Routes.js
+++ auth-portal/src/Routes.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import { Switch, Redirect, Route } from 'react-router-dom'
-import RouteWithLayout from './components/RouteWithLayout'
-
-import Page from './view/Page'
-import PageOne from './view/PageOne'
-import PageTwo from './view/PageTwo'
-
-import Main from './layouts/Main'
+import { RouteWithLayout } from './components'
+import { User, Role, Permission } from './view'
+import { Main } from './layouts'
 
 export default function Routes() {
     return (
         <Switch>
-            <RouteWithLayout exact path="/" component={Page} layout={Main} />
-            <RouteWithLayout exact path="/page-one" component={PageOne} layout={Main} />
-            <RouteWithLayout exact path="/page-two" component={PageTwo} layout={Main} />
+            <Redirect exact path="/" to="/user" />
+            <RouteWithLayout exact path="/user" component={User} layout={Main} />
+            <RouteWithLayout exact path="/role" component={Role} layout={Main} />
+            <RouteWithLayout exact path="/permission" component={Permission} layout={Main} />
         </Switch>
     )
 }
