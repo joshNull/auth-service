510a20d5677140b9653f3415768621c311c3e0e3
joshNull <josh@josh.local>
2020-03-28 13:30:25 +0800

    Material ui default theme

--- auth-portal/src/theme/index.js
+++ auth-portal/src/theme/index.js
@@ -1,4 +1,8 @@
-import { createMuiTheme } from '@material-ui/core/styles';
+import { createMuiTheme } from '@material-ui/core/styles'
+import palette from './palette'
+import typography from './typography'
+
+
 
 const theme = createMuiTheme({
     // palette,
