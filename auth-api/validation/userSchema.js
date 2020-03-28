24568b257f7d482f7354c127ea8a803863901a06
joshNull <josh@josh.local>
2020-03-28 19:39:17 +0800

    Added new fields for validation

--- auth-api/validation/userSchema.js
+++ auth-api/validation/userSchema.js
@@ -2,7 +2,7 @@ const Joi = require('@hapi/joi');
 
 const registerValidation = (data) => {
     return Joi.object({
-        username: Joi.string().min(10).required(),
+        email: Joi.string().email().required(),
         password: Joi.string().min(6).required()
     }).validate(data)
 }
