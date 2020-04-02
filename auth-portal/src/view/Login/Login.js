54d9d30e50182f8a0d131da2d06c0bbe4021a17a
joshNull <josh@josh.local>
2020-04-02 18:24:12 +0800

    Added login functionality to authenticate

--- auth-portal/src/view/Login/Login.js
+++ auth-portal/src/view/Login/Login.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
 import { Grid, Typography, TextField, Button } from '@material-ui/core'
 import { makeStyles } from '@material-ui/styles'
+import { useForm } from 'react-hook-form'
+import { AppContext } from '../../context'
 
 const useStyles = makeStyles((theme) => ({
     TextField: {
@@ -8,8 +10,53 @@ const useStyles = makeStyles((theme) => ({
     }
 }))
 
-export default function Login() {
+export default function Login(props) {
     const classes = useStyles()
+    const { register, errors, handleSubmit } = useForm()
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [app, setApp] = useContext(AppContext)
+
+    const onSubmit = (e) => {
+
+        const options = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ email: email, password: password }),
+            credentials: 'include'
+        }
+
+        fetch(app.env.api_url + 'api/login', options)
+            .then(async (response) => {
+
+                const { data } = await response.json()
+
+                // // check for error response
+                // if (!response.ok) {
+                //     // get error message from body or default to response status
+                //     const error = (data && data.message) || response.status;
+                //     return Promise.reject(error);
+                // }
+
+                setApp({
+                    ...app,
+                    authenticated: true,
+                    user: {
+                        firstName: data.firstName,
+                        lastName: data.lastName,
+                        email: data.email,
+                    }
+                })
+
+                props.history.push('/user')
+
+            })
+            .catch(error => {
+                // this.setState({ errorMessage: error });
+                console.error('There was an error!', error);
+            })
+    }
+
     return (
         <Grid
             container
@@ -26,35 +73,45 @@ export default function Login() {
                 >
                     Sign in
                 </Typography>
-                <TextField
-                    className={classes.TextField}
-                    fullWidth
-                    label="Email address"
-                    name="email"
-                    type="text"
-                    variant="outlined"
-                />
-                <TextField
-                    className={classes.TextField}
-                    fullWidth
-                    label="password"
-                    name="password"
-                    type="password"
-                    variant="outlined"
-                />
-                <Button
-                    // className={}
-                    color="primary"
-                    // disabled={}
-                    fullWidth
-                    size="large"
-                    type="submit"
-                    variant="contained"
-                >
-                    Sign in now
-                </Button>
-            </Grid>
+                <form onSubmit={handleSubmit(onSubmit)}>
 
+                    {/* FOR ENHANCEMENT */}
+                    {errors.password && <Typography>Invalid format</Typography>}
+                    {errors.email && <Typography>Invalid format</Typography>}
+
+                    <TextField
+                        className={classes.TextField}
+                        fullWidth
+                        label="Email address"
+                        name="email"
+                        type="text"
+                        variant="outlined"
+                        onChange={(e) => { setEmail(e.target.value) }}
+                        inputRef={register({ required: true })}
+                    />
+                    <TextField
+                        className={classes.TextField}
+                        fullWidth
+                        label="password"
+                        name="password"
+                        type="password"
+                        variant="outlined"
+                        onChange={(e) => { setPassword(e.target.value) }}
+                        inputRef={register({ required: true })}
+                    />
+                    <Button
+                        // className={}
+                        color="primary"
+                        // disabled={}
+                        fullWidth
+                        size="large"
+                        type="submit"
+                        variant="contained"
+                    >
+                        Sign in now
+                    </Button>
+                </form>
+            </Grid>
         </Grid>
     )
 }
