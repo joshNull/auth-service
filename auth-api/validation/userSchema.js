486e6034db8a9dc88c4b1b9bb00b975d5bb5855c
joshNull <josh@josh.local>
2020-04-02 18:15:24 +0800

    Register and login validation

--- auth-api/validation/userSchema.js
+++ auth-api/validation/userSchema.js
@@ -2,11 +2,21 @@ const Joi = require('@hapi/joi');
 
 const registerValidation = (data) => {
     return Joi.object({
+        firstName: Joi.string().required(),
+        lastName: Joi.string().required(),
+        email: Joi.string().email().required(),
+        password: Joi.string().min(6).required()
+    }).validate(data)
+}
+
+const loginValidation = (data) => {
+    return Joi.object({
         email: Joi.string().email().required(),
         password: Joi.string().min(6).required()
     }).validate(data)
 }
 
 module.exports = {
-    registerValidation
+    registerValidation,
+    loginValidation
 }
