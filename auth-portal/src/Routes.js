8e18c22e198a6db16cc1c5be023436c35e7bb0cc
joshNull <josh@josh.local>
2020-04-02 18:23:46 +0800

    Updated route to have public and private

--- auth-portal/src/Routes.js
+++ auth-portal/src/Routes.js
@@ -1,19 +1,29 @@
-import React from 'react';
-import { Switch, Redirect, Route } from 'react-router-dom'
-import { RouteWithLayout } from './components'
+import React, { useContext, useEffect } from 'react';
+import { Switch } from 'react-router-dom'
+import { ProtectedPage, PublicPage } from './components'
 import { User, Role, Permission, Login } from './view'
 import { Main, Minimal } from './layouts'
+import { AppContext } from '../src/context'
+import Cookies from 'js-cookie'
+
 
 export default function Routes() {
+    const [app, setApp] = useContext(AppContext)
+
+    useEffect(() => {
+        const tokenInCookies = Cookies.get('access-token')
+        if (tokenInCookies) {
+            setApp({ ...app, authenticated: true })
+        }
+    }, [])
+
     return (
         <Switch>
-            <Redirect exact path="/" to="/user" />
-            
-            <RouteWithLayout exact path="/login" component={Login} layout={Minimal} />
-
-            <RouteWithLayout exact path="/user" component={User} layout={Main} />
-            <RouteWithLayout exact path="/role" component={Role} layout={Main} />
-            <RouteWithLayout exact path="/permission" component={Permission} layout={Main} />
+            <PublicPage exact path="/login" component={Login} layout={Minimal} />
+            <ProtectedPage exact path="/user" component={User} layout={Main} />
+            <ProtectedPage exact path="/role" component={Role} layout={Main} />
+            <ProtectedPage exact path="/permission" component={Permission} layout={Main} />
+            {/* <Redirect exact path="/" to="/user" /> */}
         </Switch>
     )
 }
