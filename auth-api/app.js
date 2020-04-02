5c61db0f705cc9812e8ac91104cb81efd75fd075
joshNull <josh@josh.local>
2020-04-02 18:19:29 +0800

    Included whitelisting for cors origin

--- auth-api/app.js
+++ auth-api/app.js
@@ -1,15 +1,39 @@
 const express = require("express")
 const cookieParser = require('cookie-parser')
 const app = express()
+const cors = require('cors')
 const routes = require('./routes')
-
+const { verifyToken } = require('./middleware')
 const { swaggerUIServe, swaggerUISetup } = require('./docs/swagger')
+
+const whitelist = [
+    'http://localhost:3000',
+    'http://localhost:8080'
+]
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    },
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true
+}
+
+app.use(cors(corsOptions))
 
 app.use(express.json())
 app.use(cookieParser())
 
-app.use('/api/user', routes.user)
-
 app.use('/api-docs', swaggerUIServe, swaggerUISetup)
+app.use('/api', routes.login)
+
+// Protected routes
+app.use('/api/user', verifyToken, routes.user)
 
 module.exports = app
