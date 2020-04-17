62bdc7b243eb7de3659f1b60172e8d7e3869d048
joshNull <josh@josh.local>
2020-04-18 06:34:15 +0800

    User and role reference table

---
+++ auth-api/database/migrations/20200404031530-create-user-role.js
@@ -0,0 +1,32 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('user_roles', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'users', key: 'id' }
+      },
+      role_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'roles', key: 'id' }
+      },
+      date_created: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      date_updated: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('user_roles');
+  }
+};
