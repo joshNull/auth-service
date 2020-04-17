c071a2a067078219f6218f0d3525959a2171dbd0
joshNull <josh@josh.local>
2020-04-18 06:32:22 +0800

    updated validation to correspond with database

--- auth-api/validation/userSchema.js
+++ auth-api/validation/userSchema.js
@@ -2,8 +2,8 @@ const Joi = require('@hapi/joi');
 
 const registerValidation = (data) => {
     return Joi.object({
-        firstName: Joi.string().required(),
-        lastName: Joi.string().required(),
+        first_name: Joi.string().required(),
+        last_name: Joi.string().required(),
         email: Joi.string().email().required(),
         password: Joi.string().min(6).required()
     }).validate(data)
