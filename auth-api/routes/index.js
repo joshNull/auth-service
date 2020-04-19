75aa469407fbf0a7e9ada16e15c9334d47e550da
joshNull <josh@josh.local>
2020-04-20 03:54:46 +0800

    Included role and permission routes

--- auth-api/routes/index.js
+++ auth-api/routes/index.js
@@ -1,7 +1,11 @@
 const user = require("./user")
 const login = require("./login")
+const role = require("./role")
+const permission = require("./permission")
 
 module.exports = {
     user,
-    login
+    login,
+    role,
+    permission
 }
