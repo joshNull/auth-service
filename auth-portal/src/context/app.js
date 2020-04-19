e79f5c803c4cfdb0a0b879d3f75ce8ae322ea25f
joshNull <josh@josh.local>
2020-04-19 22:09:08 +0800

    app context enhancement

--- auth-portal/src/context/app.js
+++ auth-portal/src/context/app.js
@@ -3,23 +3,26 @@ import ContextDevTool from 'react-context-devtool'
 
 export const AppContext = createContext()
 
+const initialState = {
+    authenticated: false,
+    fullPageLoader: false,
+    user: {
+        firstName: "",
+        lastName: "",
+        email: "",
+    },
+    error: {
+        statusCode: "",
+        message: ""
+    },
+    settings: {},
+    env: {
+        api_url: "http://localhost:3000/"
+    }
+}
+
 export const AppProvider = (props) => {
-    const [app, setApp] = useState(
-        {
-            authenticated: false,
-            fullPageLoader: false,
-            user: {
-                firstName: "",
-                lastName: "",
-                email: "",
-            },
-            error: "",
-            settings: {},
-            env: {
-                api_url: "http://localhost:3000/"
-            }
-        }
-    )
+    const [app, setApp] = useState(initialState)
     return (
         <AppContext.Provider value={[app, setApp]}>
             <ContextDevTool context={AppContext} />
