16cb4c381f3a8972934f0c79cdf6f60930c4d4cf
joshNull <josh@josh.local>
2020-04-19 22:10:44 +0800

    Used page component

--- auth-portal/src/Routes.js
+++ auth-portal/src/Routes.js
@@ -1,29 +1,43 @@
-import React, { useContext, useEffect } from 'react';
-import { Switch } from 'react-router-dom'
-import { ProtectedPage, PublicPage } from './components'
+import React, { useEffect } from 'react';
+import { Switch, Redirect } from 'react-router-dom'
+import { Page } from './components'
 import { User, Role, Permission, Login } from './view'
 import { Main, Minimal } from './layouts'
-import { AppContext } from '../src/context'
+import { useContextHelper, useSendRequest, useReducerHelper, useFetch } from './helpers'
 import Cookies from 'js-cookie'
 
-
 export default function Routes() {
-    const [app, setApp] = useContext(AppContext)
+    const [app, setApp] = useContextHelper()
+    // const [draft, dispatch] = useReducerHelper()
+
+    // const {data} = useFetch({ endpoint: "api/user" })
+  
+    // useEffect(() => {
+    //     let userDetails = {
+    //         firstName: data.first_name,
+    //         lastName: data.last_name,
+    //         email: data.email
+    //     }
+    //     dispatch({ type: 'setUser', payload: userDetails })
+    // }, [data])
+
+    // useEffect(() => {
+    //     console.log(draft)
+    //     setApp(draft)
+    // }, [draft])
 
     useEffect(() => {
-        const tokenInCookies = Cookies.get('access-token')
-        if (tokenInCookies) {
-            setApp({ ...app, authenticated: true })
-        }
+        const auth = Cookies.get('access-token') ? true : false
+        setApp({ ...app, authenticated: auth })
     }, [])
 
     return (
         <Switch>
-            <PublicPage exact path="/login" component={Login} layout={Minimal} />
-            <ProtectedPage exact path="/user" component={User} layout={Main} />
-            <ProtectedPage exact path="/role" component={Role} layout={Main} />
-            <ProtectedPage exact path="/permission" component={Permission} layout={Main} />
-            {/* <Redirect exact path="/" to="/user" /> */}
+            <Redirect exact path="/" to="/login" />
+            <Page isLoggedIn={false} exact path="/login" component={Login} layout={Minimal} />
+            <Page exact path="/user" component={User} layout={Main} />
+            <Page exact path="/role" component={Role} layout={Main} />
+            <Page exact path="/permission" component={Permission} layout={Main} />
         </Switch>
     )
 }
