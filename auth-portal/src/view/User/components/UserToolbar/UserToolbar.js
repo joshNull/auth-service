a35ffcf826bf361cb1507456d3c26d79d98d1f21
joshNull <josh@josh.local>
2020-04-19 22:06:45 +0800

    User component

---
+++ auth-portal/src/view/User/components/UserToolbar/UserToolbar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Typography } from '@material-ui/core'
+
+
+function UserToolbar() {
+    return (<Typography>TOOLBAR</Typography>)
+}
+
+export default UserToolbar
